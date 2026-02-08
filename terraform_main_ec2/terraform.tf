5319ee1a31205a75853499acad144a3836e99472
saurabh <chanchlesh2597@gmail.com>
2026-02-09 00:55:27 +0530

    demo

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "arumullaaluruu1"
+    bucket = "arumullaaluruu101"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,19 +64,19 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-0532be01f26a3de55" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "m7i-flex.large"
 }
 
 variable "key_name" {
-  description = "EC2 keypair"
+  description = "projects-key"
   type        = string
-  default     = "us-east-1"
+  default     = "projects-key"
 }
 
 variable "instance_name" {
